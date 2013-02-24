f4197a09b99128c6e6e330a6cabefb4d8069bcf7
Matty <hello@matty.co.za>
2013-02-24 08:57:43 +0200

    Adjusts "minItems" default value to be 1. Fixes #463.

--- jquery.flexslider.js
+++ jquery.flexslider.js
@@ -955,7 +955,7 @@
     // Carousel Options
     itemWidth: 0,                   //{NEW} Integer: Box-model width of individual carousel items, including horizontal borders and padding.
     itemMargin: 0,                  //{NEW} Integer: Margin between carousel items.
-    minItems: 0,                    //{NEW} Integer: Minimum number of carousel items that should be visible. Items will resize fluidly when below this.
+    minItems: 1,                    //{NEW} Integer: Minimum number of carousel items that should be visible. Items will resize fluidly when below this.
     maxItems: 0,                    //{NEW} Integer: Maxmimum number of carousel items that should be visible. Items will resize fluidly when above this limit.
     move: 0,                        //{NEW} Integer: Number of carousel items that should move on animation. If 0, slider will move all visible items.
 
