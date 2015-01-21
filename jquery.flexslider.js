2ae8079564f7ba1a1757d35446ae5eb0a8cf8388
Tomas van Rijsse <tomas@pionect.nl>
2015-01-21 17:56:54 +0100

    Recalculate pagingCount when the window resizes

--- jquery.flexslider.js
+++ jquery.flexslider.js
@@ -556,7 +556,7 @@
       },
       resize: function() {
         if (!slider.animating && slider.is(':visible')) {
-          if (!carousel) slider.doMath();
+          slider.doMath();
 
           if (fade) {
             // SMOOTH HEIGHT:
