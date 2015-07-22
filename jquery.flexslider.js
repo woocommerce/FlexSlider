45ad4f6543a3a00e1b2920dc76439bd2bdf714dc
Curtiss Grymala <curt_grymala@yahoo.com>
2015-07-22 11:13:18 -0400

    Reverse unintentional spacing changes

--- jquery.flexslider.js
+++ jquery.flexslider.js
@@ -624,19 +624,19 @@
               if (methods.pauseInvisible.isHidden()) {
                 if(slider.startTimeout) {
                   clearTimeout(slider.startTimeout); //If clock is ticking, stop timer and prevent from starting while invisible
-                } else {
+                } else { 
                   slider.pause(); //Or just pause
                 }
               }
               else {
                 if(slider.started) {
                   slider.play(); //Initiated before, just play
-                } else {
-                  if (slider.vars.initDelay > 0) {
+                } else { 
+                  if (slider.vars.initDelay > 0) { 
                     setTimeout(slider.play, slider.vars.initDelay);
                   } else {
                     slider.play(); //Didn't init before: simply init or wait for it
-                  }
+                  } 
                 }
               }
             });
@@ -757,7 +757,7 @@
               slider.animating = false;
               slider.currentSlide = slider.animatingTo;
             }
-
+            
             // Unbind previous transitionEnd events and re-bind new transitionEnd event
             slider.container.unbind("webkitTransitionEnd transitionend");
             slider.container.bind("webkitTransitionEnd transitionend", function() {
