3b83230cf7d96ea7c575d99ab66bcf86a485c205
Alessandro Pasotti <elpaso@itopen.it>
2015-08-18 17:06:12 +0200

    Added href to allow keyboard interaction

--- jquery.flexslider.js
+++ jquery.flexslider.js
@@ -350,7 +350,7 @@
       },
       pausePlay: {
         setup: function() {
-          var pausePlayScaffold = $('<div class="' + namespace + 'pauseplay"><a></a></div>');
+          var pausePlayScaffold = $('<div class="' + namespace + 'pauseplay"><a href="#"></a></div>');
 
           // CONTROLSCONTAINER:
           if (slider.controlsContainer) {
