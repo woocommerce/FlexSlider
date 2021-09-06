900cd08a6d27c6c3bafc44b274e265b5e7986193
Daniele Scasciafratte <mte90net@gmail.com>
2021-09-06 19:31:28 +0200

    Fix #25641: Gallery thumbnails without size
    This should silence the lighthouse warnings, based on the code of https://github.com/woocommerce/woocommerce/issues/25461#issuecomment-845673185

--- jquery.flexslider.js
+++ jquery.flexslider.js
@@ -243,7 +243,11 @@
               
               item = $( '<a></a>' ).attr( 'href', '#' ).text( j );
               if ( slider.vars.controlNav === "thumbnails" ) {
-                item = $( '<img/>' ).attr( 'src', slide.attr( 'data-thumb' ) );
+                item = $( '<img/>' )
+                .attr( 'width', parseInt( slide.width() ) )
+                .attr( 'height', parseInt( slide.height() ) )
+                .attr( 'alt', slide.attr( 'alt' ) )
+                .attr( 'src', slide.attr( 'data-thumb' ) );
               }
               
               if ( '' !== slide.attr( 'data-thumb-alt' ) ) {
